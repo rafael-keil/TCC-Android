b8078406efca44262b1849f69716b4824bd1d87e
Rafael Keil <rafael.keil@cwi.com.br>
2022-01-06 11:31:13 -0300

    fazendo tela de detalhes da spell

--- app/src/main/java/br/com/cwi/tcc_android/presentation/feature/compendium/spells/spellDetail/SpellDetailFragment.kt
+++ app/src/main/java/br/com/cwi/tcc_android/presentation/feature/compendium/spells/spellDetail/SpellDetailFragment.kt
@@ -47,6 +47,22 @@ class SpellDetailFragment : Fragment() {
     }
 
     private fun setupDetailsInfo(item: Spell) {
-        binding.tvName.text = item.name
+        item.run {
+            binding.tvName.text = name
+            binding.tvLevelContent.text = level.toString()
+            binding.tvSchoolContent.text = school.name
+            binding.tvCastingTimeContent.text = castingTime
+            binding.tvRangeContent.text = range
+            binding.tvComponentsContent.text = components.joinToString(separator = ", ")
+            binding.tvDurationContent.text = duration
+            binding.tvClassesContent.text = classes.joinToString(separator = ", ") { it.name }
+            binding.tvDesc.text = desc.joinToString(separator = "\n      ", prefix = "      ")
+
+            higherLevel?.let {
+                binding.higherLevelCotent.text = it.joinToString(separator = "\n      ", prefix = "      ")
+                binding.higherLevel.visibility = View.VISIBLE
+                binding.higherLevelCotent.visibility = View.VISIBLE
+            }
+        }
     }
 }
