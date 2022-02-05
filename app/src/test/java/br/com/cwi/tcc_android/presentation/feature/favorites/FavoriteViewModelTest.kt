d8ff6acce497b035dd533aaf8f1856c7eec65558
Rafael Keil <rafael.keil@cwi.com.br>
2022-02-05 14:05:30 -0300

    arrumando testes

--- app/src/test/java/br/com/cwi/tcc_android/presentation/feature/favorites/FavoriteViewModelTest.kt
+++ app/src/test/java/br/com/cwi/tcc_android/presentation/feature/favorites/FavoriteViewModelTest.kt
@@ -6,13 +6,19 @@ import br.com.cwi.tcc_android.domain.entity.BaseCompendiumItem
 import br.com.cwi.tcc_android.domain.repository.DndLocalRepository
 import br.com.cwi.tcc_android.fixture.EquipmentFixture
 import br.com.cwi.tcc_android.fixture.SpellFixture
-import io.mockk.*
+import io.mockk.every
+import io.mockk.justRun
+import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
-import org.junit.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
@@ -25,12 +31,14 @@ class FavoriteViewModelTest {
 
     private lateinit var viewModel: FavoriteViewModel
     private val dndLocalRepository: DndLocalRepository = mockk()
-    private val favoritesObserver = mockk<Observer<List<BaseCompendiumItem>>>(relaxed = true)
+    private val favoritesObserver: Observer<List<BaseCompendiumItem>> = mockk(relaxed = true)
     private val dispatcher = TestCoroutineDispatcher()
 
     @Before
     fun setup() {
         Dispatchers.setMain(dispatcher)
+        viewModel = FavoriteViewModel(dndLocalRepository)
+        viewModel.favorites.observeForever(favoritesObserver)
     }
 
     @After
@@ -41,52 +49,45 @@ class FavoriteViewModelTest {
 
     @Test
     fun `when view model fetch favorites then return all favorites`() {
-        viewModel = FavoriteViewModel(dndLocalRepository)
-        viewModel.favorites.observeForever(favoritesObserver)
-        val spells = SpellFixture.getSpellList()
-        val equipments = EquipmentFixture.getEquipmentList()
-
-        coEvery { dndLocalRepository.getAllSpells() } returns spells
-        coEvery { dndLocalRepository.getAllEquipments() } returns equipments
+        every { dndLocalRepository.getAllSpells() } returns SpellFixture.getSpellList()
+        every { dndLocalRepository.getAllEquipments() } returns EquipmentFixture.getEquipmentList()
 
         viewModel.fetchFavorites()
 
-        val favorites = spells + equipments
-        //favorites.map { it.isFavorite = true }
-
-        coVerify { favoritesObserver.onChanged(favorites) }
-        Assert.assertEquals(viewModel.favorites.value, favorites)
+        verify {
+            favoritesObserver.onChanged(
+                match { item ->
+                    item.all { it.isFavorite }
+                }
+            )
+        }
     }
 
     @Test
     fun `when view model setFavorite spell then remove spell`() {
         val spell = SpellFixture.getSpell()
-        val spells = SpellFixture.getSpellList()
-        val equipments = EquipmentFixture.getEquipmentList()
 
         justRun { dndLocalRepository.remove(spell) }
-        every { dndLocalRepository.getAllSpells() } returns spells
-        every { dndLocalRepository.getAllEquipments() } returns equipments
+        every { dndLocalRepository.getAllSpells() } returns SpellFixture.getSpellList()
+        every { dndLocalRepository.getAllEquipments() } returns EquipmentFixture.getEquipmentList()
 
         viewModel.setFavorite(spell)
 
         verify { dndLocalRepository.remove(spell) }
-        verify { viewModel.fetchFavorites() }
+        verify { favoritesObserver.onChanged(any()) }
     }
 
     @Test
     fun `when view model setFavorite equipment then remove equipment`() {
         val equipment = EquipmentFixture.getEquipment()
-        val spells = SpellFixture.getSpellList()
-        val equipments = EquipmentFixture.getEquipmentList()
 
         justRun { dndLocalRepository.remove(equipment) }
-        every { dndLocalRepository.getAllSpells() } returns spells
-        every { dndLocalRepository.getAllEquipments() } returns equipments
+        every { dndLocalRepository.getAllSpells() } returns SpellFixture.getSpellList()
+        every { dndLocalRepository.getAllEquipments() } returns EquipmentFixture.getEquipmentList()
 
         viewModel.setFavorite(equipment)
 
         verify { dndLocalRepository.remove(equipment) }
-        verify { viewModel.fetchFavorites() }
+        verify { favoritesObserver.onChanged(any()) }
     }
 }
